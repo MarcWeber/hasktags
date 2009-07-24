a9039e552ef973765ef219a7dd5450ce3afa2faa
marco-oweber <marco-oweber@gmx.de>
2009-07-24 16:32:46 -0300

    enhance test runner script
    Ignore-this: d0ebab0d6b99c938a22f6d00e564c831
    
    darcs-hash:20090724193246-ce738-1e3a81a5ced291c5574b17a5ce14e7ca64f3b551.gz

--- testcases/test.sh
+++ testcases/test.sh
@@ -2,7 +2,7 @@
 
 checkToBeFound(){
   hasktags=$1
-  file=`echo $2/*`
+  file=`echo $2/*.*` # exclude tags file
   echo
   echo
   echo "running test on $file"
@@ -13,8 +13,9 @@ checkToBeFound(){
   [ -f tags ] && rm tags
   $hasktags $file &> /dev/null
   for i in $toBeFound; do
-    grep -l $i tags 2>&1 > /dev/null || echo "!->> tag $i was not found"
+    grep -l "^$i" tags 2>&1 > /dev/null || echo "!->> tag $i was not found"
   done
+  cp tags $2/tags
 }
 
 # compile hasktags
@@ -22,6 +23,6 @@ cd ..
 ghc --make hasktags.hs -o hasktags
 cd -
 
-for f in `find . -maxdepth 1 -type d `; do
+for f in `find . -mindepth 1 -maxdepth 1 -type d`; do
   checkToBeFound ../hasktags $f
 done
