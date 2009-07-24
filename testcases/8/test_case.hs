ef6d2c2fdcb9e755565bf6ee354223bbd0fcffe8
marco-oweber <marco-oweber@gmx.de>
2009-07-24 16:34:37 -0300

    new testcase
    Ignore-this: 187dc88288172358e5c7919f56c6830c
    
    darcs-hash:20090724193437-ce738-3bfe0c02b9986d8735035a1d64505805dc87e034.gz

---
+++ testcases/8/test_case.hs
@@ -0,0 +1,22 @@
+-- to be found Main
+-- to be found ABC
+-- to be found ABCD
+-- to be found @=?
+-- to be found @=:
+-- to be found dummy
+-- to be found main
+module Main where
+
+data ABC a = ABC a
+class (Show a) => (ABCD a) where
+  abcshow :: (ABC a)
+
+(@=?), (@=:) :: (Eq a, Show a) => a -> a -> Int
+
+expected @=? actual = undefined
+expected @=: actual = undefined
+
+dummy = "a"
+
+main = print "abc"
+
